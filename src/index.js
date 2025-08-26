de96409fa5602626edb18ee88bd2ce9ad08462be
HeitorRafael <hitoor.d@gmail.com>
2025-08-26 17:33:30 -0300

    Mudança para Postgres

--- src/index.js
+++ src/index.js
@@ -25,6 +25,7 @@ app.get('/usuario', (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`);
 });
---
+++ src/syncDatabase.js
@@ -0,0 +1,20 @@
+const sequelize = require('./config/database');
+
+// Importar todos os modelos aqui
+const User = require('./models/user');
+
+const syncDatabase = async () => {
+    try {
+        await sequelize.authenticate();
+        console.log('Conexão com o banco de dados estabelecida com sucesso.');
+
+        // Sincroniza todos os modelos com o banco de dados
+        await sequelize.sync({ alter: true });
+        console.log('Modelos sincronizados com o banco de dados.');
+    } catch (error) {
+        console.error('Erro ao conectar com o banco de dados:', error);
+        process.exit(1);
+    }
+};
+
+module.exports = syncDatabase;
